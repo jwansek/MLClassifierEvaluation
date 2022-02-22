e6787864a3b2a776e1f7f9e4a09b42503e7d0c07
jwansek <eddie.atten.ea29@gmail.com>
2022-02-22 19:42:57 +0000

    started analysing metrics

--- Eval.java
+++ Eval.java
@@ -3,9 +3,11 @@ import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.BayesNet;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.bayes.NaiveBayesMultinomial;
+import weka.classifiers.bayes.NaiveBayesMultinomialText;
 import weka.classifiers.lazy.IBk;
 import weka.classifiers.lazy.KStar;
 import weka.classifiers.meta.AdaBoostM1;
+import weka.classifiers.meta.ClassificationViaRegression;
 import weka.classifiers.meta.LogitBoost;
 import weka.classifiers.trees.*;
 import weka.core.Instance;
@@ -58,8 +60,8 @@ public class Eval {
         }
     }
 
-    public static UCIDataset[] getDatasets() throws FileNotFoundException, IOException {
-        File[] datasetPaths = Paths.get(".", "UCI").toFile().listFiles();
+    public static UCIDataset[] getDatasets(File datasetPath) throws FileNotFoundException, IOException {
+        File[] datasetPaths = datasetPath.listFiles();
         UCIDataset[] datasets = new UCIDataset[datasetPaths.length];
         for (int i = 0; i < datasetPaths.length; i++) {
             datasets[i] = new UCIDataset(datasetPaths[i].toString());
@@ -96,14 +98,61 @@ public class Eval {
             }
             System.out.println(Paths.get(".", classifierName, dataset.getName() + ".csv").toString());
             System.out.println(numCorrect + " / " + tester.numInstances() + " correct");
+            writer.close();
         }
+    }
+
+    public static <T extends Classifier> void getClassifierMetrics(T[] classifiers, UCIDataset dataset) throws Exception {
+        FileWriter writer = new FileWriter(Paths.get(".", "MetricsByDataset", dataset.getName() + ".csv").toFile());
+            writer.write("Classifier Name,Correct,Incorrect,Kappa,Total Cost,Average Cost,Mean Absolute Error");
+            writer.write("Root Mean Squared Error,Coverate,True Positive Rate,False Positive Rate,Precision,Recall,F Measure");
+            writer.write("Matthew's Correlation Coefficient,Area under ROC,Area under PRC");
+            writer.write("\n");
+
+        for (T classifier : classifiers) {
+            String[] s = classifier.getClass().toGenericString().split("\\.");
+            String classifierName = s[s.length - 1];
+
+            Instances trainer = dataset.getTrainInstances();
+            Instances tester = dataset.getTestInstances();
+            classifier.buildClassifier(trainer);
+            Evaluation evaluation = new Evaluation(trainer);
+            evaluation.evaluateModel(classifier, tester);
+
+            // writer.write(classifierName + ",");
+            // writer.write(evaluation.correct() + ",");
+            // writer.write(evaluation.incorrect() + ",");
+            // writer.write(evaluation.kappa() + ",");
+            // writer.write(evaluation.totalCost() + ",");
+            // writer.write(evaluation.avgCost() + ",");
+            // writer.write(evaluation.meanAbsoluteError() + ",");
+            // writer.write(evaluation.rootMeanSquaredError() + ",");
+            // writer.write(evaluation.relativeAbsoluteError() + ",");
+            // writer.write(evaluation.rootRelativeSquaredError() + ",");
+            // writer.write(evaluation.coverageOfTestCasesByPredictedRegions() + ",");
+            // writer.write(evaluation.truePositiveRate(tester.numAttributes() - 1) + ",");
+            // writer.write(evaluation.falsePositiveRate(tester.numAttributes() - 1) + ",");
+            // writer.write(evaluation.precision(tester.numAttributes() - 1) + ",");
+            // writer.write(evaluation.recall(tester.numAttributes() - 1) + ",");
+            // writer.write(evaluation.fMeasure(tester.numAttributes() - 1) + ",");
+            // writer.write(evaluation.matthewsCorrelationCoefficient(tester.numAttributes() - 1) + ",");
+            // writer.write(evaluation.areaUnderROC(tester.numAttributes() - 1) + ",");
+            // writer.write(evaluation.areaUnderPRC(tester.numAttributes() - 1) + ",");
+
+            System.out.println(classifierName);
+            System.out.println(evaluation.toSummaryString());
+            
+            writer.write("\n");
+        }
+        writer.close();
     }
 
     public static void main(String[] args) {
         
         try {
-            getClassifierProbabilities(new AdaBoostM1(), getDatasets());
+            // getClassifierProbabilities(new AdaBoostM1(), getDatasets(Paths.get(".", "UCI").toFile()));
 
+            getClassifierMetrics(new Classifier[]{new J48(), new AdaBoostM1(), new BayesNet(), new DecisionStump(), new HoeffdingTree(), new IBk(), new LMT(), new LogitBoost(), new NaiveBayes(), new NaiveBayesMultinomialText(), new RandomForest(), new RandomTree(), new REPTree()}, new UCIDataset(Paths.get(".", "UCIContinuous", "bank").toString()));
 
 
 
